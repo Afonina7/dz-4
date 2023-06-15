760eda91a99505b479a11a0e0500a3b491d01e27
EvgeniiAfonin <evgeniy.afonin7@gmail.com>
2023-06-15 18:34:09 +0300

    home work lesson5

---
+++ src/main/java/ClassApp.java
@@ -0,0 +1,11 @@
+public class ClassApp {
+
+    public static void main(String[] args) {
+        ClassLesson5 cl = new ClassLesson5();
+        cl.myNumbers();
+
+        int number2 = 10;
+        int result = cl.myNumbers() + number2;
+        System.out.println("Checks return value: " + result);
+    }
+}
---
+++ src/main/java/ClassLesson5.java
@@ -0,0 +1,9 @@
+public class ClassLesson5 {
+
+    int number = 7;
+
+    public int myNumbers() {
+        System.out.println("Number from ClassLesson5: " + number);
+        return number;
+    }
+}
